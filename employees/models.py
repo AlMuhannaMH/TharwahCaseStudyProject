4bd0ba8d2fd2057f1f3ad1b8ea2ab4d1e8665215
Mohammed AlMuhanna <almuhannamh@outlook.com>
2021-12-22 11:30:18 +0300

    Define the Django Model

--- employees/models.py
+++ employees/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=30, blank=False, default='')
+    last_name = models.CharField(max_length=30, blank=False, default='')
+    job_title = models.CharField(max_length=70, blank=False, default='')
+    manager = models.ForeignKey("self", blank=True, null=True)
+    department = models.ForeignKey("Department")
+
+
+class Department(models.Model):
+    hod = models.ForeignKey("Employee")
+    name = models.CharField(max_length=100)
