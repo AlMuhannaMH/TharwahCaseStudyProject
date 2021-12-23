82e7da1e7ea6c73119492986a1f838c4d5e3e3c7
Mohammed AlMuhanna <almuhannamh@outlook.com>
2021-12-23 10:19:31 +0300

    Migrate Data Model to PostgreSQL database

--- TharwahCaseStudyProject/settings.py
+++ TharwahCaseStudyProject/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'tharwahemployeesdb',
         'USER': 'postgres',
-        'PASSWORD': 'password',
+        'PASSWORD': 'toor',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
--- employees/models.py
+++ employees/models.py
@@ -1,17 +1,19 @@
 from django.db import models
 
 # Create your models here.
-from django.db import models
 
 
 class Employee(models.Model):
     first_name = models.CharField(max_length=30, blank=False, default='')
     last_name = models.CharField(max_length=30, blank=False, default='')
     job_title = models.CharField(max_length=70, blank=False, default='')
-    manager = models.ForeignKey("self", blank=True, null=True)
-    department = models.ForeignKey("Department")
+    manager = models.ForeignKey(
+        'self', on_delete=models.CASCADE, blank=True, null=True)
+    department = models.ForeignKey(
+        'Department', on_delete=models.CASCADE, blank=False, default='')
 
 
 class Department(models.Model):
-    hod = models.ForeignKey("Employee")
-    name = models.CharField(max_length=100)
+    hod = models.ForeignKey(
+        'Employee', on_delete=models.CASCADE, blank=True, null=True, related_name='heads_of_department')
+    name = models.CharField(max_length=100, blank=False, default='')
