610de44f44fdd0fcd62963a0b49a22a3d9b8542b
Mohammed AlMuhanna <almuhannamh@outlook.com>
2021-12-23 11:22:47 +0300

    Define Routes to Views functions

---
+++ employees/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+from employees import views
+
+urlpatterns = [
+    url(r'^api/employees$', views.employee_list),
+    url(r'^api/employees/(?P<pk>[0-9]+)$', views.employee_detail),
+]
