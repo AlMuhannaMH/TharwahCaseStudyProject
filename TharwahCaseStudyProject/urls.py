86be9e557de4121b36a68bbb199f48616ef84d88
Mohammed AlMuhanna <almuhannamh@outlook.com>
2021-12-23 12:09:10 +0300

    test api

--- TharwahCaseStudyProject/urls.py
+++ TharwahCaseStudyProject/urls.py
@@ -13,8 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.urls import include, re_path
 
 urlpatterns = [
-    url(r'^', include('employees.urls')),
+    re_path(r'^', include('employees.urls')),
 ]
--- employees/urls.py
+++ employees/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import url
+from django.urls import re_path
 from employees import views
 
 urlpatterns = [
-    url(r'^api/employees$', views.employee_list),
-    url(r'^api/employees/(?P<pk>[0-9]+)$', views.employee_detail),
+    re_path(r'^api/employees$', views.employee_list),
+    re_path(r'^api/employees/(?P<pk>[0-9]+)$', views.employee_detail),
 ]
