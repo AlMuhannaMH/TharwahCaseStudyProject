018eeb95f20241bde3da9c21dddcf447eef8e9d6
Mohammed AlMuhanna <almuhannamh@outlook.com>
2021-12-23 11:16:47 +0300

    Create Serializer class for Data Model

---
+++ employees/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from employees.models import Employee, Department
+
+
+class EmployeeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Employee
+        fields = ('first_name'
+                  'last_name',
+                  'job_title',
+                  'department'
+                  'manager')
+
+
+class DepartmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Department
+        fields = ('hod'
+                  'name')
