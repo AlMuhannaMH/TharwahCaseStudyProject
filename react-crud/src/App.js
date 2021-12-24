46caddf4ba5308b4428212b9c93265272d82471e
Mohammed AlMuhanna <almuhannamh@outlook.com>
2021-12-24 17:42:22 +0300

    Add Navbar and Initialize Axios

--- react-crud/src/App.js
+++ react-crud/src/App.js
@@ -11,30 +11,28 @@ import EmployeesList from "./components/EmployeesList";
 const { Header, Content, Footer } = Layout;
 function App() {
   return (
-    <div>
+  <div>
     <Layout>
-    <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
-      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
-        <Menu.Item key="1"><Link to={"/employees"}>Employees</Link></Menu.Item>
-        <Menu.Item key="2"><Link to={"/add"}>Add</Link></Menu.Item>
-      </Menu>
-    </Header>
-    <Content className="site-layout" style={{ padding: '0 50px', marginTop: 64 }}>
-      <div className="site-layout-background" style={{ padding: 24, minHeight: 380 }}>
-        Content
-      </div>
-    </Content>
-    <Footer style={{ textAlign: 'center' }}>Tharwah ©2021 Created by MHM</Footer>
-  </Layout>
-      {/* <div className=""> */}
-      <div>
-        {/* <Switch>
+      <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+          <Menu.Item key="1"><Link to={"/employees"}>Employees</Link></Menu.Item>
+          <Menu.Item key="2"><Link to={"/add"}>Add</Link></Menu.Item>
+        </Menu>
+      </Header>
+      <Content className="site-layout" style={{ padding: '0 50px', marginTop: 64 }}>
+        <div className="site-layout-background" style={{ padding: 24, minHeight: 380 }}>
+          <div>
+          {/* <Switch>
           <Route exact path={["/", "/employees"]} component={EmployeesList} />
           <Route exact path="/add" component={AddEmployee} />
           <Route path="/employees/:id" component={Employee} />
-        </Switch> */}
-      </div>
-    </div>
+          </Switch> */}
+          </div>
+        </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>Tharwah ©2021 Created by MHM</Footer>
+    </Layout>
+  </div>
   );
 }
 
