f495e022753eff60772743c296c78a37b2edd136
Mohammed AlMuhanna <almuhannamh@outlook.com>
2021-12-27 20:39:27 +0300

    fix antd

--- react-crud/src/App.js
+++ react-crud/src/App.js
@@ -1,23 +1,34 @@
 import React from "react";
-import { Routes, Route, Link } from "react-router-dom";
 import "antd/dist/antd.min.css";
-import { Layout, Menu } from "antd";
 import "./App.css";
-
-import AddEmployee from "./components/AddEmployee";
-import Employee from "./components/Employee";
-import EmployeesList from "./components/EmployeesList";
+import { Layout, Menu } from "antd";
 
 const { Header, Content, Footer } = Layout;
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
+    <Layout className="layout">
+      <Header>
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={["1"]}
+          style={{ lineHeight: "64px" }}
+        >
+          <Menu.Item key="1">Employees</Menu.Item>
+          <Menu.Item key="2">Add</Menu.Item>
+        </Menu>
+      </Header>
+      <Content style={{ padding: "0 50px" }}>
+        <div style={{ background: "#fff", padding: 24, minHeight: 280 }}>
+          {/* <UserRecord /> */}
+        </div>
+      </Content>
+
+      <Footer style={{ textAlign: "center" }}>
+        Tharwah ©2021 Created by MHM
+      </Footer>
+    </Layout>
   );
 }
 
--- react-crud/src/index.js
+++ react-crud/src/index.js
@@ -1,15 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, HashRouter } from "react-router-dom";
-
+import "./index.css";
 import App from "./App";
+
+// import { BrowserRouter as Router, HashRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
 reportWebVitals();
